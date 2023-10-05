712555f3c6a82e2ab68c4e5ebb7c56a2046d8bc2
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-05 19:25:55 +0300

    string

--- First C-Sharp/Program.cs
+++ First C-Sharp/Program.cs
@@ -10,7 +10,13 @@ namespace First_C_Sharp
     {
         static void Main(string[] args)
         {
+
+            string name = "Mister";
+            int age = 10;
+
+
             Console.WriteLine("Excited to start off with C# ");
+            Console.WriteLine("My name is " + name + "and I am " + age + "years old");
             Console.WriteLine("Hey mahn, C#.");
             Console.WriteLine("Give me 6 months.");
             Console.ReadLine();
