108f2c7b5dd3c6f47d65d21de96198e3001630ff
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-05 19:18:23 +0300

    Add project files.

---
+++ First C-Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Excited to start off with C# ");
+            Console.WriteLine("Hey mahn, C#.");
+            Console.WriteLine("Give me 6 months.");
+            Console.ReadLine();
+        }
+    }
+}
